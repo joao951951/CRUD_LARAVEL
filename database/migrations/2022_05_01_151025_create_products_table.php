0b13702fde914d80354857a33d4517deb51abe02
Joao Paulo Carnellossi dos Santos <joao951951>
2022-05-01 12:48:34 -0300

    v1.1

---
+++ database/migrations/2022_05_01_151025_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',30);
+            $table->text('description')->nullable;
+            $table->string('quantity');
+            $table->decimal('price, 5,2');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/', 'AdminController@getIndex');
+    Route::get('projetos', 'ProductController@Index');
+    Route::get('projetos/inserir', 'ProductController@getInserir');
+    Route::post('projetos/inserir', 'ProductController@postInserir');
+    Route::get('projetos/editar/{id}', 'ProductController@getEditar');
+    Route::post('projetos/editar/{id}', 'ProductController@postEditar');
+    Route::post('projetos/deletar/{id}', 'ProductController@postDeletar');
 });
